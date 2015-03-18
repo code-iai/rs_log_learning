e6e8b046ec2322a508ccdae18442868413fe84d9
Andre Lübken <aluebken@cs.uni-bremen.de>
2015-03-18 17:33:38 +0100

    cleanup

--- src/algorithms/DecisionTreeAlgorithm.cpp
+++ src/algorithms/DecisionTreeAlgorithm.cpp
@@ -22,25 +22,24 @@ DecisionTreeAlgorithm::~DecisionTreeAlgorithm()
 MPIdentifiable DecisionTreeAlgorithm::process(std::vector<MPIdentifiable> referenceSet, MPIdentifiable query)
 {
     iai_rs::util::StopWatch clock;
-    outInfo("========================== dt start ====================");
 
-    float *trainingData = new float[10*referenceSet.size()]; // data for training
+    float *initialTrainingData = new float[10*referenceSet.size()]; // data for training
     float *testData = new float[10]; // query data
 
     for(int i = 0; i < referenceSet.size(); ++i)
     {
-        trainingData[i*10] = (float)referenceSet[i].getGeometry().getBoundingBoxVolume();
+        initialTrainingData[i*10] = (float)referenceSet[i].getGeometry().getBoundingBoxVolume();
         if(!referenceSet[i].getSemColor().getColorMapping().empty())
         {
-            trainingData[i*10 + 1] = referenceSet[i].getSemColor().getColorMapping()["white"];
-            trainingData[i*10 + 2] = referenceSet[i].getSemColor().getColorMapping()["yellow"];
-            trainingData[i*10 + 3] = referenceSet[i].getSemColor().getColorMapping()["red"];
-            trainingData[i*10 + 4] = referenceSet[i].getSemColor().getColorMapping()["black"];
-            trainingData[i*10 + 5] = referenceSet[i].getSemColor().getColorMapping()["grey"];
-            trainingData[i*10 + 6] = referenceSet[i].getSemColor().getColorMapping()["blue"];
-            trainingData[i*10 + 7] = referenceSet[i].getSemColor().getColorMapping()["green"];
-            trainingData[i*10 + 8] = referenceSet[i].getSemColor().getColorMapping()["magenta"];
-            trainingData[i*10 + 9] = referenceSet[i].getSemColor().getColorMapping()["cyan"];
+            initialTrainingData[i*10 + 1] = referenceSet[i].getSemColor().getColorMapping()["white"];
+            initialTrainingData[i*10 + 2] = referenceSet[i].getSemColor().getColorMapping()["yellow"];
+            initialTrainingData[i*10 + 3] = referenceSet[i].getSemColor().getColorMapping()["red"];
+            initialTrainingData[i*10 + 4] = referenceSet[i].getSemColor().getColorMapping()["black"];
+            initialTrainingData[i*10 + 5] = referenceSet[i].getSemColor().getColorMapping()["grey"];
+            initialTrainingData[i*10 + 6] = referenceSet[i].getSemColor().getColorMapping()["blue"];
+            initialTrainingData[i*10 + 7] = referenceSet[i].getSemColor().getColorMapping()["green"];
+            initialTrainingData[i*10 + 8] = referenceSet[i].getSemColor().getColorMapping()["magenta"];
+            initialTrainingData[i*10 + 9] = referenceSet[i].getSemColor().getColorMapping()["cyan"];
         }
         else
         {
@@ -66,29 +65,23 @@ MPIdentifiable DecisionTreeAlgorithm::process(std::vector<MPIdentifiable> refere
         outError("No SemColorMap in this reference identifiable");
     }
 
-    float *X10trainingData = new float[10*10*referenceSet.size()]; // data for training
-    std::copy(trainingData, trainingData + 10*referenceSet.size(), X10trainingData);
-    std::copy(trainingData, trainingData + 10*referenceSet.size(), X10trainingData + 1*10*referenceSet.size());
-    std::copy(trainingData, trainingData + 10*referenceSet.size(), X10trainingData + 2*10*referenceSet.size());
-    std::copy(trainingData, trainingData + 10*referenceSet.size(), X10trainingData + 3*10*referenceSet.size());
-    std::copy(trainingData, trainingData + 10*referenceSet.size(), X10trainingData + 4*10*referenceSet.size());
-    std::copy(trainingData, trainingData + 10*referenceSet.size(), X10trainingData + 5*10*referenceSet.size());
-    std::copy(trainingData, trainingData + 10*referenceSet.size(), X10trainingData + 6*10*referenceSet.size());
-    std::copy(trainingData, trainingData + 10*referenceSet.size(), X10trainingData + 7*10*referenceSet.size());
-    std::copy(trainingData, trainingData + 10*referenceSet.size(), X10trainingData + 8*10*referenceSet.size());
-    std::copy(trainingData, trainingData + 10*referenceSet.size(), X10trainingData + 9*10*referenceSet.size());
+    float *trainingData = new float[mm*10*referenceSet.size()]; // final training data
+    for(int i = 0; i < mm; ++i)
+    {
+        std::copy(initialTrainingData, initialTrainingData + 10*referenceSet.size(), trainingData + i*10*referenceSet.size());
+    }
 
     cv::Mat testMat(1, 10, CV_32FC1, testData);
-    cv::Mat trainingMat(10*referenceSet.size(), 10, CV_32FC1, X10trainingData);
+    cv::Mat trainingMat(10*referenceSet.size(), 10, CV_32FC1, trainingData);
 
-    std::cout << "TrainingData: " << trainingMat << std::endl;
-    std::cout << "testData: " << testMat << std::endl;
+    //std::cout << "TrainingData: " << trainingMat << std::endl;
+    //std::cout << "testData: " << testMat << std::endl;
 
     outInfo("before trainingClasses");
     cv::Mat trainingClasses = labelData(referenceSet);
     cv::Mat testClasses = labelData(referenceSet); // TODO: not needed, get just the prediction
 
-    std::cout << "trainingClasses: " << trainingClasses << std::endl;
+    //std::cout << "trainingClasses: " << trainingClasses << std::endl;
 
     float resultValue = decisiontree(trainingMat, trainingClasses, testMat, testClasses);
     float confidence = 1 - abs(resultValue - (size_t)round(resultValue));
@@ -137,15 +130,10 @@ cv::Mat DecisionTreeAlgorithm::labelData(std::vector<MPIdentifiable> &referenceS
     for(std::map<int,NameShape>::iterator it = indexToAnnotationData.begin(); it != indexToAnnotationData.end(); ++it)
     {
         labels.at<float>(labelIndex, 0) = (float)it->first;
-        labels.at<float>(labelIndex + indexToAnnotationData.size() * 1, 0) = (float)it->first;
-        labels.at<float>(labelIndex + indexToAnnotationData.size() * 2, 0) = (float)it->first;
-        labels.at<float>(labelIndex + indexToAnnotationData.size() * 3, 0) = (float)it->first;
-        labels.at<float>(labelIndex + indexToAnnotationData.size() * 4, 0) = (float)it->first;
-        labels.at<float>(labelIndex + indexToAnnotationData.size() * 5, 0) = (float)it->first;
-        labels.at<float>(labelIndex + indexToAnnotationData.size() * 6, 0) = (float)it->first;
-        labels.at<float>(labelIndex + indexToAnnotationData.size() * 7, 0) = (float)it->first;
-        labels.at<float>(labelIndex + indexToAnnotationData.size() * 8, 0) = (float)it->first;
-        labels.at<float>(labelIndex + indexToAnnotationData.size() * 9, 0) = (float)it->first;
+        for(int i = 0; i < mm; ++i)
+        {
+            labels.at<float>(labelIndex + indexToAnnotationData.size() * i, 0) = (float)it->first;
+        }
         ++labelIndex;
     }
 
@@ -164,7 +152,7 @@ float DecisionTreeAlgorithm::decisiontree(cv::Mat& trainingData, cv::Mat& traini
 
     cv::Mat var_type(11, 1, CV_8UC1, data);
 
-    outInfo("starting dtree");
+    outInfo("starting training");
 
 
     dtree.train(trainingData,CV_ROW_SAMPLE, trainingClasses, cv::Mat(), cv::Mat(), var_type, cv::Mat(),
@@ -179,6 +167,8 @@ float DecisionTreeAlgorithm::decisiontree(cv::Mat& trainingData, cv::Mat& traini
                           0));  // no priors
     cv::Mat predicted(testClasses.rows, 1, CV_32F); // zu gross....
     predicted.zeros(testClasses.rows, 1, CV_32F);
+
+    outInfo("predicting input query");
     for (int i = 0; i < testData.rows; i++)
     {
         const cv::Mat sample = testData.row(i);
@@ -186,51 +176,15 @@ float DecisionTreeAlgorithm::decisiontree(cv::Mat& trainingData, cv::Mat& traini
         predicted.at<float> (i, 0) = prediction->value;
     }
 
-    std::cout << "testClasses = " << testClasses << std::endl;
-    std::cout << "predicted = " << predicted << std::endl;
+    //std::cout << "testClasses = " << testClasses << std::endl;
+    //std::cout << "predicted = " << predicted << std::endl;
     //std::cout << "Accuracy_{TREE} = " << evaluate(predicted, testClasses) << std::endl;
-    std::cout << "Var importance mat = " << dtree.getVarImportance() << std::endl;
+    //std::cout << "Var importance mat = " << dtree.getVarImportance() << std::endl;
 
     outInfo("prediction value: " <<  predicted.at<float>(0,0));
     delete[] data;
 
     return predicted.at<float>(0,0);
-}
-
-int DecisionTreeAlgorithm::f(float x, float y, int equation)
-{
-    if(x < 1) return 1;
-    if(x < 2 && x > 1) return 2;
-    if(x < 3 && x > 2) return 3;
-    if(x < 4 && x > 3) return 4;
-    if(x < 5 && x > 4) return 5;
-    if(x < 6 && x > 5) return 6;
-    if(x < 7 && x > 6) return 7;
-    if(x < 8 && x > 7) return 8;
-    if(x < 9 && x > 8) return 9;
-    if(x < 10 && x > 9) return 10;
-    else return 0;
-
-}
-
-float DecisionTreeAlgorithm::evaluate(cv::Mat& predicted, cv::Mat& actual)
-{
-    assert(predicted.rows == actual.rows);
-    int t = 0;
-    int f = 0;
-    for(int i = 0; i < actual.rows; i++) {
-        float p = predicted.at<float>(i,0);
-        float a = actual.at<float>(i,0);
-        std::cout << "evaluate: predicted/actual: " << p << " / " << a << "  " << (int)round(p) << "/" << (int)round(a) << std::endl;
-        if((int)(round(p) == (int)(round(a))))
-        {
-            t++;
-        }
-        else {
-            f++;
-        }
-    }
-    return (t * 1.0) / (t + f);
 }
 
 } /* namespace rs_log_learn */
